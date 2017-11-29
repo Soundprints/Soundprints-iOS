45466d821cb67915e7777717738c49f42ce60a58
Svit Zebec <svit.zebec@gmail.com>
2017-11-29 10:32:45 +0100

    Created initial MainMap screen

---
+++ Soundprints/Soundprints/Source/User Interface/Main Map/MainMapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainMapViewController.swift
+//  Soundprints
+//
+//  Created by Svit Zebec on 29/11/2017.
+//  Copyright © 2017 Kamino. All rights reserved.
+//
+
+import UIKit
+
+class MainMapViewController: BaseViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet private weak var mapView: MGLMapView?
+
+}
